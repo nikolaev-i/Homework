f41c282cf4126edd9717313de3028ceb5dceae2c
Ivan <eng.ivan.nikolaev@gmail.com>
2023-02-22 21:42:31 +0200

    Calculator requirments

---
+++ Homework-4/calculator.py
@@ -0,0 +1,17 @@
+# Write a Calculator program with the following functionality:
+# - The user chooses the operation 
+# - add, subtract, multiply, divide   = check whether the choice is valid   = every operation should be a separate function 
+# - The user enters the numbers for the operation   = check for valid inputs 
+# - only numbers allowed 
+# - The user is presented with the result from the operation or an error message   = division by zero error handling   = try different formatting for the floating point numbers (1,2,3,4 numbers after the decimal) 
+# - The user can choose whether to proceed with a new calculation or exit the program 
+# Example: Select operation. 
+# 1.Add 
+# 2.Subtract 
+# 3.Multiply 
+# 4.Divide 
+# Enter choice: (1/2/3/4): 3 
+# Enter first number: 15 
+# Enter second number: 14 15.0 * 14.0 = 210.0
+#  Would you like to do a new calculation? 
+#  (yes/no): no 
