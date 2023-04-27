1e43b85f2914175777d7c888c89043e6582f0c4d
IvaN <eng.ivan.nikolaev@gmail.com>
2023-04-27 14:41:54 +0300

    vm's off by default

--- Homework-19/main.tf
+++ Homework-19/main.tf
@@ -10,11 +10,12 @@ terraform {
 # There are currently no configuration options for the provider itself.
 
 resource "virtualbox_vm" "node" {
-  count     = 5 
+  count     = 2 
   name      = format("node-%02d", count.index + 1)
   image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
   cpus      = 2
   memory    = "512 mib"
+  status = "poweroff"
 
 
   network_adapter {
